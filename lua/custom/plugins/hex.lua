7e08bfb746bb9b851a2592cd2674f56efa5528a9
Matthieu Monnier <matthieu.monnier@orange.com>
2023-10-17 21:04:56 +0200

    add custom configuration

---
+++ lua/custom/plugins/hex.lua
@@ -0,0 +1,15 @@
+return {
+    "RaafatTurki/hex.nvim",
+    config = function()
+      require 'hex'.setup {
+        dump_cmd = 'xxd -g 1 -u',
+        assemble_cmd = 'xxd -r',
+        is_buf_binary_pre_read = function()
+        end,
+        is_buf_binary_post_read = function()
+          -- logic that determines if a buffer contains binary data or not
+          -- must return a bool
+        end,
+      }
+    end,
+}
---
+++ lua/custom/plugins/nvim-training.lua
@@ -0,0 +1,5 @@
+return {
+  {
+    "Weyaaron/nvim-training",
+  }
+}
